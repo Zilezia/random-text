2ddfe3ef2e3ddaf383ed5d951b8cb71350262870
Zilezia <151420333+Zilezia@users.noreply.github.com>
2023-11-27 11:45:05 +0000

    Add files via upload

---
+++ a.js
@@ -0,0 +1,19 @@
+
+var randomQuote = ["quote1", "quote2", "quote3", "quote4", "quote5", "quote6", "quote7", "quote8", "quote9", "quote10"];
+var randomAuthor = ["author1", "author2", "author3", "author4", "author5", "author6", "author7", "author8", "author9", "author10",];
+
+
+
+$(document).ready(function () {
+    var n = Math.floor(Math.random() * randomQuote.length);
+
+    $("#text p").html('"' + randomQuote[n] + '"');
+    $("#author p").html('- ' + randomAuthor[n]);
+});
+
+$("#new-quote").click(function () {
+    var n = Math.floor(Math.random() * randomQuote.length);
+
+    $("#text p").html('"' + randomQuote[n] + '"');
+    $("#author p").html('- ' + randomAuthor[n]);
+});
